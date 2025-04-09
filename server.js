2c8070c3bc8a7b14b4615df65cc6ea0e3b502676
Suleewan <165354516+Suleewan@users.noreply.github.com>
2025-04-09 14:16:03 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+//import ....
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const {readdirSync} = require('fs')
+const cors = require('cors')
+const { PrismaClient } = require('@prisma/client');
+const bodyParser = require('body-parser');
+const prisma = new PrismaClient();
+const { PrismaClient } = require('./prisma/generated/prisma-client');
+
+
+
+
+app.use(morgan('dev'))
+app.use(express.json());
+app.use(cors())
+app.use(bodyParser.json());
+
+
+
+readdirSync('./routes')
+.map((c)=> app.use('/api', require('./routes/'+ c)) )
+
+
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+prisma.$connect()
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((error) => {
+    console.error('Database connection failed:', error);
+});
